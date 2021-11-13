fded1716cd1c6b24cf090d32bb8a34ba260d72f4
Crowdbotics <team@crowdbotics.com>
2021-11-13 18:00:19 +0000

    Update models

--- backend/led_lighting_31982/wsgi.py
+++ backend/led_lighting_31982/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'led_lighting_31982.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "led_lighting_31982.settings")
 
 application = get_wsgi_application()
