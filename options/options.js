f9461ca35c7f8a5bd0d7165798596b0a0d990071
Crowdbotics <team@crowdbotics.com>
2021-11-13 17:50:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "led_lighting_31982",
+  url: "https://led_lighting_31982.botics.co",
+  api: "https://led_lighting_31982.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
